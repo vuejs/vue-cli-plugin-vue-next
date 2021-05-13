267d9771dcde6f79fa6ca117c91cb3138506da99
ygj6 <yuangongji@foxmail.com>
2021-05-13 08:43:52 +0800

    split app mount expression
    createApp().mount() => app=createApp();app.mount()

--- generator/codemods/global-api/create-app-mount.js
+++ generator/codemods/global-api/create-app-mount.js
@@ -7,13 +7,21 @@ module.exports = function createAppMount(context) {
   const { j, root } = context
 
   // new Vue(...).$mount()
-  const mountCalls = root.find(j.CallExpression, n => {
-    return (
-      n.callee.type === 'MemberExpression' &&
-      n.callee.property.name === '$mount' &&
-      n.callee.object.type === 'NewExpression' &&
-      n.callee.object.callee.name === 'Vue'
-    )
+  const mountCalls = root.find(j.ExpressionStatement, {
+    expression: {
+      type: 'CallExpression',
+      callee: {
+        type: 'MemberExpression',
+        object: {
+          type: 'NewExpression',
+          callee: {
+            type: 'Identifier',
+            name: 'Vue'
+          }
+        },
+        property: { type: 'Identifier', name: '$mount' }
+      }
+    }
   })
 
   if (!mountCalls.length) {
@@ -23,16 +31,26 @@ module.exports = function createAppMount(context) {
   const addImport = require('../utils/add-import')
   addImport(context, { imported: 'createApp' }, 'vue')
 
-  mountCalls.replaceWith(({ node }) => {
-    let rootProps = node.callee.object.arguments[0]
-    const el = node.arguments[0]
+  const rootProps = mountCalls.at(0).get().node.expression.callee.object
+    .arguments
+  mountCalls.insertBefore(
+    j.variableDeclaration('const', [
+      j.variableDeclarator(
+        j.identifier('app'),
+        j.callExpression(j.identifier('createApp'), rootProps)
+      )
+    ])
+  )
 
-    return j.callExpression(
-      j.memberExpression(
-        j.callExpression(j.identifier('createApp'), [rootProps]),
-        j.identifier('mount')
-      ),
-      [el]
+  const args = mountCalls.at(0).get().node.expression.arguments
+  mountCalls.insertBefore(
+    j.expressionStatement(
+      j.callExpression(
+        j.memberExpression(j.identifier('app'), j.identifier('mount'), false),
+        args
+      )
     )
-  })
+  )
+
+  mountCalls.remove()
 }
